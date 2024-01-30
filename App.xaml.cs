2c277c5313a8e920f095f9b0c74d51a0be3ee4a0
Jeremy Wei <jeremywei009@gmail.com>
2024-01-31 08:17:39 +1100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (MainWindow is MainWindow main)
+            {
+                // Ensure the tray icon is hidden on exit
+                main.trayIcon.Visible = false;
+            }
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ UserListWindows.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for UserListWindows.xaml
+    /// </summary>
+    public partial class UserListWindow : Window
+    {
+        public UserListWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateUserList(List<string> userNames)
+        {
+            UserListBox.ItemsSource = userNames;
+        }
+    }
+}
