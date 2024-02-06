41ea1ad0148e945ae53035baa010c5687c676648
Jeremy Wei <jeremywei009@gmail.com>
2024-02-06 14:43:18 +1100

    Add Alert window, alert type

---
+++ AlertWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for AlertWindow.xaml
+    /// </summary>
+    /// 
+   
+    public partial class AlertWindow : Window
+    {
+        private DispatcherTimer timer;
+        private bool isRed = false;
+        public AlertWindow()
+        {
+            InitializeComponent();
+            StartFlashingText();
+        }
+
+        private void StartFlashingText()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // Set the flashing interval (1 second in this example)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Toggle the color
+            isRed = !isRed;
+            AlertTextBlock.Foreground = isRed ? new SolidColorBrush(Colors.White) : new SolidColorBrush(Colors.Black);
+        }
+    }
+}
---
+++ SoundPlayerHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Timers;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class SoundPlayerHelper
+    {
+        private static MediaPlayer player = new MediaPlayer();
+        private static Uri uri = new Uri("Resources/converted_Siren.wav",UriKind.Relative);
+        //private static Timer stopTimer = new Timer(10000); // 10 seconds
+
+        public static void PlaySiren()
+        {
+            player.MediaFailed += (s, e) =>
+            {
+                MessageBox.Show($"Media Failed: {e.ErrorException.Message}");
+            };
+
+            player.Volume = 1;
+            player.Open(uri);
+            player.Play();
+
+            //stopTimer.Elapsed += StopTimer_Elapsed;
+            //stopTimer.Start();
+        }
+
+        //private static void StopTimer_Elapsed(object sender, ElapsedEventArgs e)
+        //{
+        //    player.Stop();
+        //    stopTimer.Stop();
+        //}
+    }
+}
